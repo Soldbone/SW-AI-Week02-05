4df58769781abfbfb1e0d44c531092f7ef9c84cc
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-08 20:40:20 +0900

    solve(하 - 브론즈1): 평균은 넘겠지 문제 풀기
    - 배열 - 평균은 넘겠지 (백준 브론즈1)
    - 문제 링크: https://www.acmicpc.net/problem/4344

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,25 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+C = int(input())
+
+
+def get_ratio_of_above_average_students(students: list) -> float:
+    # 평균을 구한다
+    num_of_students = students[0]
+    average = (sum(students) - num_of_students) / num_of_students
+
+    # 평균을 넘는 학생의 숫자를 구한다
+    num_of_above_average_students = 0
+    for i in range(1, num_of_students + 1):
+        if students[i] > average:
+            num_of_above_average_students += 1
+
+    # 전체 학생 수에서 평균을 넘는 학생의 비율을 백분율로 계산한다
+    ratio = num_of_above_average_students / num_of_students * 100
+
+    return ratio
+
+
+for _ in range(C):
+    student_scores = list(map(int, input().split()))
+    print(f"{get_ratio_of_above_average_students(student_scores):.3f}%")
