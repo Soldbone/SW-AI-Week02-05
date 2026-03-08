cc74d4f3d3382230d10744d759bf0d8570901709
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-08 20:56:41 +0900

    solve(하 - 브론즈2): 문자열 반복 문제 풀기
    # 문자열 - 문자열 반복 (백준 브론즈2)
    # 문제 링크: https://www.acmicpc.net/problem/2675

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,18 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+T = int(input())
+
+for _ in range(T):
+    # 반복 횟수와 문자열 입력 받기
+    R, word = input().split()
+    R = int(R)
+
+    # 결과 문자열 초기화
+    result_word = ""
+
+    # 문자열의 각 문자를 반복 횟수만큼 반복 생성해서 결과 문자열에 더하기
+    for char in word:
+        result_word += char * R
+
+    # 결과 문자열 출력
+    print(result_word)
