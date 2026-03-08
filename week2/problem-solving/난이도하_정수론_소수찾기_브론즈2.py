37cb2e4ab783e0c6a244f4cedb666d9771db8d6b
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-08 18:46:58 +0900

    solve(하 - 브론즈2): 소수 찾기 문제 풀기
    - 정수론 - 소수 찾기 (백준 브론즈2)
    - 문제 링크: https://www.acmicpc.net/problem/1978

--- week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
+++ week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
@@ -1,2 +1,25 @@
 # 정수론 - 소수 찾기 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/1978
+
+
+def is_prime(n: int) -> bool:
+    if n < 2:
+        return False
+
+    if n % 2 == 0:
+        return n == 2
+
+    for i in range(3, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+N = int(input())
+nums = list(map(int, input().split()))
+count = 0
+for n in nums:
+    if is_prime(n):
+        count += 1
+
+print(count)
