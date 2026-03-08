e19b59b544f114519fc23bc15aab398e76603dd3
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-08 18:00:49 +0900

    solve(하 - 브론즈3): 최댓값 문제 풀기
    - 파이썬 문법 - 최댓값 (백준 브론즈3)
    - 링크: https://www.acmicpc.net/problem/2562

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,14 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+N = 9
+max_num = int(input())
+max_location = 1
+for i in range(1, N):
+    num = int(input())
+    if num > max_num:
+        max_location = i + 1
+        max_num = num
+
+print(max_num)
+print(max_location)
