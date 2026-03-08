3de8d55b933961d3188e88736410d3287dbbe615
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-08 22:38:04 +0900

    refactor(하 - 브론즈1): 단어공부 코드 개선 w/ AI
    AI를 전적으로 활용함

--- week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,30 +1,45 @@
 # 문자열 - 단어 공부 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/1157
 
-word = input()
-count_for_char = {}
+# word = input().upper()
+# count_for_char = {}
+# max_count = 1
+# # 등록 시 최대값을 기억하도록 약간 개선
+# for char in word:
+#     ignore_case_char = char
+#     if ignore_case_char not in count_for_char:
+#         count_for_char[ignore_case_char] = 1
+#     else:
+#         count_for_char[ignore_case_char] += 1
+#         count = count_for_char[ignore_case_char]
+#         if count > max_count:
+#             max_count = count
+
+# # 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력
+# # 순회해서 가장 큰 값을 갖는 키를 리스트에 저장하고 리스트의 길이를 확인한다.
+# most_freq_chars = []
+# for char, count in count_for_char.items():
+#     if count == max_count:
+#         most_freq_chars.append(char)
+
+# if len(most_freq_chars) == 1:
+#     print(most_freq_chars[0])
+# else:
+#     print("?")
+
+"""
+ChatGPT의 도움을 받아 개선한 버전
+"""
+word = input().upper()
+
+counts_for_alpha = [0] * 26
+
 for char in word:
-    ignore_case_char = char.upper()
-    if ignore_case_char not in count_for_char:
-        count_for_char[ignore_case_char] = 1
-    else:
-        count_for_char[ignore_case_char] += 1
+    counts_for_alpha[ord(char) - ord("A")] += 1
 
-# 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력
-# 무식하게 풀어보자:
-# 순회해서 가장 큰 값을 찾는다.
-# 순회해서 가장 큰 값을 갖는 키의 개수를 확인한다.
-max_count = count_for_char[word[0].upper()]
-for char, count in count_for_char.items():
-    if count > max_count:
-        max_count = count
+max_count = max(counts_for_alpha)
 
-most_freq_chars = []
-for char, count in count_for_char.items():
-    if count == max_count:
-        most_freq_chars.append(char)
-
-if len(most_freq_chars) == 1:
-    print(most_freq_chars[0])
+if counts_for_alpha.count(max_count) == 1:
+    print(chr(counts_for_alpha.index(max_count) + ord("A")))
 else:
     print("?")
