17790f8c33ea8c40ba01f31f3f9db8ebbe398239
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-10 15:53:38 +0900

    solve(중 - 실버2): 차이를 최대로 문제 풀기 w/ AI
    - 문제 접근은 했는데 순열 경우의 수 구하는 법을 몰라서 AI 학습모드 사용
    # 완전탐색 - 차이를 최대로 (백준 실버2)
    # 문제 링크: https://www.acmicpc.net/problem/10819

--- week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
+++ week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
@@ -1,2 +1,51 @@
 # 완전탐색 - 차이를 최대로 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10819
+
+N = int(input())
+nums = list(map(int, input().split()))
+
+# 모든 경우의 수를 만든다.  -> HOW?
+# 모든 경우의 수마다 식의 값을 계산하여 최대값을 찾는다.
+
+# visited = [False] * N
+
+# max = -1
+
+
+# def perm(path: list):
+#     global max
+#     if len(path) == N:
+#         acc = 0
+#         for i in range(len(path) - 1):
+#             acc += abs(path[i] - path[i + 1])
+
+#         if acc > max:
+#             max = acc
+#         return
+
+#     for i in range(len(nums)):
+#         if not visited[i]:
+#             path.append(nums[i])
+#             visited[i] = True
+#             perm(path)
+#             path.pop()
+#             visited[i] = False
+
+
+# perm([])
+# print(max)
+
+
+
+## permutations 라이브러리 사용
+from itertools import permutations
+
+answer = -1
+
+for perm in permutations(nums):
+    total = 0
+    for i in range(N - 1):
+        total += abs(perm[i] - perm[i + 1])
+    answer = max(answer, total)
+
+print(answer)
