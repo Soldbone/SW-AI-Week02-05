ccfab925228de0551915c65890d5bcd9895727c7
Soldbone <1472ezs@gmail.com>
2026-03-11 02:18:52 +0900

    solve(중 - 골드5): 하노이 탑 문제 풀기

--- week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
+++ week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
@@ -1,2 +1,22 @@
 # 재귀함수 - 하노이 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1914
+N = int(input())
+
+# 하노이 옮기는 개수 점화식.
+# n번째 블록 위의 T(n-1)개를 옮기고  n번째 블록을 제일 오른쪽으로 옮기고 n
+count = 2**N - 1
+print(count)
+
+def hanoi(src, dest, via, n):
+    if n > 20:
+        return
+
+    if n == 1:
+        print(src, dest)
+        return
+    else:
+        hanoi(src, via, dest, n-1)
+        print(src, dest)
+        hanoi(via, dest, src, n-1)
+
+hanoi("1", "3", "2", N)
