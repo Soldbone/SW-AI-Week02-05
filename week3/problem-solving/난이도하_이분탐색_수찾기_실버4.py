ae4430a13e6f03ee38aa127ca852470f4d46f92b
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-15 23:28:15 +0900

    basic(하): 수찾기 - 실버4 찾기

--- week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
+++ week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
@@ -1,2 +1,10 @@
 # 이분탐색 - 수 찾기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/1920
+
+N = int(input())
+A = dict.fromkeys(input().split(), 0)
+M = int(input())
+nums = input().split()
+
+for word in nums:
+    print(int(word in A))
