7eb4b62e45058816f84cc68e29279d38d595cc5b
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-15 22:44:01 +0900

    solve(하): 카드2 - 실버4 풀기
    - 요세푸스 문제와 관련됨
    - AI를 이용해서 코드 개선

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,18 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+"""
+요세푸스 문제와 연관 있다고 한다.
+공식이 있다.
+"""
+
+from collections import deque
+
+N = int(input())
+deck = deque(range(1, N + 1))
+
+while N > 1:
+    deck.popleft()
+    deck.append(deck.popleft())
+    N -= 1
+
+print(deck[0])
