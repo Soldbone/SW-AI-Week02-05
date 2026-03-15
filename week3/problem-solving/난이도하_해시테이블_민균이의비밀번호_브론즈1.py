dc46db07f0615e517ec4c8b781f518effc19a41f
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-15 21:53:10 +0900

    solve(하): 민균이의 비밀번호 - 브론즈1 풀기
    - AI로 조건 개선

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,18 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+rainbow = set()
+
+for _ in range(N):
+    word = input().strip()
+    if (word[::-1] in rainbow) or (word == word[::-1]):
+        break
+    else:
+        rainbow.add(word)
+
+word_length = len(word)
+print(word_length, word[word_length // 2])
