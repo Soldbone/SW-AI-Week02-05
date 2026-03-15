fedf9cce25e590211046cad8c0d6b6560d872ede
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-16 00:20:37 +0900

    solve(하): 스택 - 실버4 풀기 w/ 코드 참고
    - 생각보다 오류를 잡아내기 쉽지 않아서 이전에 짰던 코드 참고

--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,67 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+
+
+class Node:
+    def __init__(self, val: int):
+        self.val = val
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self._top: Node = None
+        self.length = 0
+
+    def push(self, item: int) -> None:
+        node = Node(item)
+
+        if self.empty():
+            self._top = node
+        else:
+            node.next = self._top
+            self._top = node
+        self.length += 1
+
+    def pop(self) -> int:
+        if self.empty():
+            return -1
+
+        node = self._top
+        self._top = node.next
+        # gc
+        self.length -= 1
+        return node.val
+
+    def size(self) -> int:
+        return self.length
+
+    def empty(self) -> bool:
+        return int(self._top is None)
+
+    def top(self) -> int:
+        if self.empty():
+            return -1
+        else:
+            return self._top.val
+
+
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+stack = Stack()
+for _ in range(N):
+    command = input().split()
+    match command[0]:
+        case "push":
+            stack.push(int(command[1]))
+        case "pop":
+            print(stack.pop())
+        case "size":
+            print(stack.size())
+        case "empty":
+            print(stack.empty())
+        case "top":
+            print(stack.top())
