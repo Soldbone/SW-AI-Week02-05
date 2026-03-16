b808717eeafa11044e099aa2bcf81809d019c6f9
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-16 16:49:17 +0900

    solve(중): 세 수의 합 - 골드4 w/ AI
    - 아이디어는 다 냈는데 구체적인 구현 과정에서 어려움을 겪음

--- week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
+++ week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
@@ -1,2 +1,28 @@
 # 해시 테이블 - 세 수의 합 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2295
+
+# 두 수의 합을 저장해놓고 뒤에서부터 저장된 두 수의 합을 뺀 값이 후보에 있는지 확인해보면 어떨까
+
+import sys
+
+input = sys.stdin.readline
+candidate = set()
+nums = []
+N = int(input())
+for _ in range(N):
+    nums.append(int(input()))
+
+nums.sort()
+for i in range(N):
+    for j in range(i, N):
+        candidate.add(nums[i] + nums[j])
+
+BREAK_NESTED_FOR = False
+for i in range(N - 1, -1, -1):
+    for j in range(i - 1, -1, -1):
+        if nums[i] - nums[j] in candidate:
+            print(nums[i])
+            BREAK_NESTED_FOR = True
+            break
+    if BREAK_NESTED_FOR:
+        break
