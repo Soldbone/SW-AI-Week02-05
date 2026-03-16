e46b1a6532a76bb3ef8a8b1141d2120487f0cf88
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-16 17:28:06 +0900

    solve(중): 곱셈 - 실버1 풀기 w/ AI
    - 나머지 연산 최적화 방법을 몰라서 AI의 도움을 받음

--- week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,36 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+
+
+a, b, c = map(int, input().split())
+
+
+def pow_modulo(num, n):
+    if n == 1:
+        return num % c
+
+    half_exp = pow_modulo(num, n // 2)
+    if n % 2 == 0:
+        return (half_exp * half_exp) % c
+    else:
+        return (num * half_exp * half_exp) % c
+
+
+print(pow_modulo(a, b))
+
+
+
+## 반복문 버전
+a, b, c = map(int, input().split())
+
+result = 1
+num = a % c
+n = b
+
+while n > 0:
+    if n % 2 == 1:
+        result = (result * num) % c
+    num = (num * num) % c
+    n //= 2
+
+print(result)
