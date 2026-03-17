0eab49b7163777c5645e3bc748c438fb7e5332ab
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-17 16:43:58 +0900

    coop(중): 두 용액 - 골드5 문제 풀기

--- week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
+++ week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
@@ -1,2 +1,26 @@
 # 이분탐색 - 두 용액 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2470
+
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+
+liquid = sorted(list(map(int, input().split())))
+
+i = 0
+j = len(liquid) - 1 - i
+min = float("inf")
+min_index = (None, None)
+while i < j:
+    mixture = liquid[i] + liquid[j]
+    if abs(mixture) < abs(min):
+        min = mixture
+        min_index = (i, j)
+
+    if mixture >= 0:
+        j -= 1
+    else:
+        i += 1
+
+print(liquid[min_index[0]], liquid[min_index[1]])
