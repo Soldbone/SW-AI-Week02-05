d382939eb0803e0382f440edb38b250ff54ae649
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-20 16:48:35 +0900

    solve(basic): 이진탐색 트리 풀기
    - AI로 반복문 코드 보고 나서 혼자서 짜 봄

--- week4/basic/02_bst.py
+++ week4/basic/02_bst.py
@@ -33,30 +33,56 @@
 - target == root.value → 찾음!
 """
 
+
 class TreeNode:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
+
 def search_bst(root, target):
     """
     BST에서 값 검색
-    
+
     Args:
         root: 트리 루트
         target: 찾을 값
-    
+
     Returns:
         True/False
     """
-    # TODO: root가 None이면 False 반환
-    pass
-    
-    # TODO: 값을 찾으면 True 반환
-    ## target이 작으면 왼쪽 서브트리에서 검색
-    ## target이 크면 오른쪽 서브트리에서 검색
-    pass
+    ## 재귀
+    # # root가 None이면 False 반환
+    # if root is None:
+    #     return False
+
+    # # 값을 찾으면 True 반환
+    # if target == root.value:
+    #     return True
+
+    # ## target이 작으면 왼쪽 서브트리에서 검색
+    # elif target < root.value:
+    #     return search_bst(root.left, target)
+
+    # ## target이 크면 오른쪽 서브트리에서 검색
+    # else:  # target > root.value
+    #     return search_bst(root.right, target)
+
+    ## 반복문
+    cur = root
+
+    while cur:
+        if target < cur.value:
+            cur = cur.left
+        elif target > cur.value:
+            cur = cur.right
+        else:  # target == cur.value
+            return True
+
+    # if cur is None:
+    return False
+
 
 # 테스트 케이스
 if __name__ == "__main__":
@@ -71,13 +97,11 @@ if __name__ == "__main__":
     root.right = TreeNode(7)
     root.left.left = TreeNode(2)
     root.left.right = TreeNode(4)
-    
+
     print("=== 이진 검색 트리 ===")
     print("트리 구조: 5를 루트로 하는 BST")
-    
+
     test_values = [2, 4, 5, 6, 7]
     for val in test_values:
         result = search_bst(root, val)
         print(f"값 {val} 검색: {result}")
-
-
