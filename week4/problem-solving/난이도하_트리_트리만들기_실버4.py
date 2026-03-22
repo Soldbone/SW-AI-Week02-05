f731cda522f5d5dfffbf2a05e7694d9ff5bfe226
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-23 00:42:45 +0900

    solve(하): 트리 만들기 - 실버4 w/ AI
    - 개념은 알겠는데 구현을 못 하겠어서 학습모드 사용

--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,24 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+
+import sys
+
+# 중심 노드를 정한다. (0으로 정하자.)
+n, m = map(int, input().split())
+
+edges = []
+
+# 중심 노드에 리프 노드를 M개 붙인다.
+for i in range(1, m + 1):
+    edges.append((0, i))
+
+# 지금까지 사용된 노드는 M+1개이다.
+# 나머지 노드는 N - (M+1)개이다.
+# 나머지 노드는 임의의 리프 노드 뒤에 연속적으로 붙인다.
+prev = 1
+for i in range(m + 1, n):
+    edges.append((prev, i))
+    prev = i
+
+for edge in edges:
+    sys.stdout.write(f"{edge[0]} {edge[1]}\n")
