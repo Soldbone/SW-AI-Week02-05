b1d87c586242dbba256cccec66a0d44336116ec1
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-23 01:11:51 +0900

    solve(하): 바이러스 - 실버3 풀기
    - 상근이의 여행과 같은 방식으로 풀이

--- week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
@@ -1,2 +1,29 @@
 # 그래프, DFS, BFS - 바이러스 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/2606
+
+import sys
+
+input = sys.stdin.readline
+
+num_com = int(input())
+num_com_in_net = int(input())
+
+graph = [[] for _ in range(num_com + 1)]
+visited = [False] * (num_com + 1)
+for _ in range(num_com_in_net):
+    src, dest = map(int, input().split())
+    graph[src].append(dest)
+    graph[dest].append(src)
+
+count = 0
+def dfs(graph, node, visited):
+    global count
+    visited[node] = True
+
+    for neighbor in graph[node]:
+        if not visited[neighbor]:
+            count += 1
+            dfs(graph, neighbor, visited)
+
+dfs(graph, 1, visited)
+print(count)
