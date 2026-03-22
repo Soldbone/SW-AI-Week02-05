58a44ee4db37b133cda0cc04d8c7a825e2e5aee2
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-22 21:47:56 +0900

    solve(하): 상근이의 여행 - 실버4 풀기
    - 비효율적인 풀이

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,44 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+
+import sys
+
+input = sys.stdin.readline
+
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        run_test_case()
+
+
+def run_test_case():
+    N, M = map(int, input().split())
+
+    graph = [[] for _ in range(N + 1)]
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+
+    find_least_airplane_path(N, graph)
+
+
+def find_least_airplane_path(N, graph: list[list[int]]):
+    to_visit = [True] * (N + 1)
+    count = 0
+
+    def _dfs(node):
+        to_visit[node] = False
+        nonlocal count
+
+        for neighbor in graph[node]:
+            if to_visit[neighbor]:
+                count += 1
+                _dfs(neighbor)
+
+    _dfs(1)
+    print(count)
+
+
+main()
