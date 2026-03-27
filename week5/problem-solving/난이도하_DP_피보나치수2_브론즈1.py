10c14cab87d6b65717b905cf3926bf5cfa27e5e2
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-28 00:06:21 +0900

    solve(하): 피보나치 수2 - 브론즈1 풀기

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,19 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+def fibo(n, memo=None):
+    if memo is None:
+        memo = dict()
+    
+    if n <= 1:
+        return n
+    
+    if n in memo:
+        return memo[n]
+    
+    memo[n] = fibo(n-1, memo) + fibo(n-2, memo)
+    return memo[n]
+
+
+n = int(input())
+print(fibo(n))
