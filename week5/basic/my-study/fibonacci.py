3a7e698d08e8cc832ee6fe321f0dd9beab1bcfde
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-28 13:49:25 +0900

    refactor(basic): 계단 오르기 개선
    배열을 생성하는 과정 자체에서 기저조건 예외를 처리하도록 개선

--- week5/basic/my-study/fibonacci.py
+++ week5/basic/my-study/fibonacci.py
@@ -1,3 +1,10 @@
+"""
+###################
+    피보나치 수열
+###################
+"""
+
+
 def fib_tabulation(n):
     if n <= 1:
         return n
@@ -24,4 +31,37 @@ def fib_rolling(n):
 
 
 # print(fib_tabulation(50))
-print(fib_rolling(50))
+# print(fib_rolling(50))
+
+
+"""
+###################
+    계단 오르기
+###################
+"""
+
+
+def climb_stairs(n):
+    """
+    계단 오르기 (상향식 DP)
+
+    Args:
+        n: 계단의 수
+
+    Returns:
+        n번째 계단까지 오르는 방법의 수
+    """
+    # TODO: dp 배열 생성 및 초기화
+    dp = [0] * (n + 2)
+    dp[0] = dp[1] = 1
+
+    # TODO: 작은 문제부터 차례로 계산
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
+
+
+for i in range(1, 11):
+    result = climb_stairs(i)
+    print(f"{i}번 계단: {result}가지")
