3f6298723aa5cee3a4d38c85c47fb6c5bf4ef61a
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-30 14:36:30 +0900

    solve(하): 동전0 - 실버4 풀기

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,19 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+coins = []
+for _ in range(N):
+    coins.append(int(input()))
+
+total_count = 0
+coins = reversed(coins)
+for coin in coins:
+    count, K = divmod(K, coin)
+    total_count += count
+    if K == 0:
+        break
+print(total_count)
