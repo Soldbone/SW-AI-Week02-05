66738e0a4c4a5d4d8c7e930184a4523c5f07ebcb
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-31 23:32:15 +0900

    refactor(중): 평범한 배낭 개선 - herry612 코드 참고

--- week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
+++ week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
@@ -4,8 +4,7 @@ N, K = map(int, input().split())
 items = [tuple(map(int, input().split())) for _ in range(N)]
 dp = [0] * (K + 1)
 
-for i in range(len(items)):
-    for w in range(K, 0, -1):
-        if w >= items[i][0]:
-            dp[w] = max(dp[w], dp[w - items[i][0]] + items[i][1])
+for weight, value in items:
+    for w in range(K, weight - 1, -1):
+        dp[w] = max(dp[w], dp[w - weight] + value)
 print(dp[K])
