f090aa47ffc5a412a6354ba23e3d0dfcbddf7be5
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-03-31 23:22:14 +0900

    solve(중): 평범한 배낭-골드5 풀기

--- week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
+++ week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
@@ -1,2 +1,11 @@
 # DP - 평범한 배낭 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/12865
+N, K = map(int, input().split())
+items = [tuple(map(int, input().split())) for _ in range(N)]
+dp = [0] * (K + 1)
+
+for i in range(len(items)):
+    for w in range(K, 0, -1):
+        if w >= items[i][0]:
+            dp[w] = max(dp[w], dp[w - items[i][0]] + items[i][1])
+print(dp[K])
